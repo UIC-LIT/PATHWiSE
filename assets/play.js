eb8358a2ab72558ec5e66abcb64f9e340bb9f248
Md Anisur Rahman <anistuhin@gmail.com>
2025-04-30 17:18:57 -0500

    Local offline audio from the robot

--- assets/play.js
+++ assets/play.js
@@ -276,9 +276,9 @@
 
     function sendToRobot(text, behavior, clip) {
         executeBehavior(behavior);
-        //var audioBase = "https://" + computerIP + "/assets/audios/"; // computer based server to serve the audios
-        var audioBase = "https://anistuhin.com/misty/"; // internet based server to serve the audios
-        //var audioBase = ""; // robot based file system to serve the audios
+        //var audioBase = "https://" + computerIP + "/assets/audios/"; // still has bug: computer based server to serve the audios
+        // var audioBase = "https://anistuhin.com/misty/"; // internet based server to serve the audios
+        var audioBase = ""; // robot based file system to serve the audios
         console.log("Sent to misty to play: " + audioBase + clip + ".mp3");
         if (isProduction) {
             Promise.race([
