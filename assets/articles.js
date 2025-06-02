18689063ed16ee6fc5fcb892a2dc9697f1c48743
Md Anisur Rahman <anistuhin@gmail.com>
2025-06-02 11:38:04 -0500

    Update articles.js

--- assets/articles.js
+++ assets/articles.js
@@ -10,37 +10,37 @@ var totalCommentCount  = 11;
 var studentsList = [{
         'name': '1',
         'id': 'student-1',
-        'current': 'Human Blood'
+        'current': 'What are Antibodies'
     },
     {
         'name': '2',
         'id': 'student-2',
-        'current': 'What are Antibodies'
+        'current': 'Justin Beiber and Lyme Disease'
     },
     {
         'name': '3',
         'id': 'student-3',
-        'current': 'Meet the Microbes'
+        'current': 'Justin Beiber and Lyme Disease'
     },
     {
         'name': '4',
         'id': 'student-4',
-        'current': 'Justin Beiber and Lyme Disease'
+        'current': 'Meet the Microbes'
     },
     {
         'name': '5',
         'id': 'student-5',
-        'current': 'Justin Beiber and Lyme Disease'
+        'current': 'Human Blood'
     },
     {
         'name': '6',
         'id': 'student-6',
-        'current': 'Meet the Microbes'
+        'current': 'What are Antibodies'
     },
     {
         'name': '7',
         'id': 'student-7',
-        'current': 'Human Blood'
+        'current': 'Meet the Microbes'
     }
 ];
 
